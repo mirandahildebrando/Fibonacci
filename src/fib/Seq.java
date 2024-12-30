9236f71f389d4ac58ba9a2be7dbba7b795686360
mirandahildebrando <mirandahildebrando5@gmail.com>
2024-12-30 12:03:05 -0300

    change made

--- src/fib/Seq.java
+++ src/fib/Seq.java
@@ -7,16 +7,19 @@ public class Seq {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value for the sequence: ");
 		
 		int f1 = 0;
 		int f2 = 1;
 		int limit = input.nextInt();
+		
 		
 		while(f2 < limit) {
 			int fn = f1 + f2;
 			if (fn > limit) 
 				break;
 				
+			
 			System.out.println(fn);
 			f1 = f2;
 			f2 = fn;
